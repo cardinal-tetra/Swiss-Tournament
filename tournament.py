2192bcdaf584527d2b139fa2bf39720567dfb9dc
Peter Zhang <peterzhang@MacBook.local>
2016-06-14 22:56:53 +1000

    optimised tournament.py further

--- tournament.py
+++ tournament.py
@@ -6,80 +6,57 @@
 import psycopg2
 
 class Query():
-    """Define class for efficiency in database connection"""
+    """Define class for efficiency in database interaction"""
     def __init__(self):
         self.connection = psycopg2.connect("dbname=tournament")
         self.cursor = self.connection.cursor()
     
-    def execute(self, sql_string, commit = True):
-        self.cursor.execute(sql_string)
+    def execute(self, sql_string, placeholder = (), commit = True):
+        self.cursor.execute(sql_string, placeholder)
         
         if commit == False:
-            return self
+            return self.cursor.fetchall()
         else:
             self.connection.commit()
-            self.close()
+            
+        self.close()
 
-    def security(self):
-        return self
-    
     def close(self):
         self.cursor.close()
         self.connection.close()
         
 def deleteMatches():
-    """Clears matches from database"""
+    """Clears all records from matches table"""
     Query().execute("DELETE FROM matches")
 
 def deletePlayers():
-    """Clear players from database"""
+    """Clears all records from players table"""
     Query().execute("DELETE FROM players")
 
 def countPlayers():
     """Find total number of players"""
-    object = Query().execute("SELECT COUNT(*) FROM players", False)
-    result = object.cursor.fetchone()
-    object.close()
-    return result[0]
+    return Query().execute("SELECT COUNT(*) FROM players", commit = False)[0][0]
 
 def registerPlayer(name):
-    """Add new record to players, protecting against SQL injection attacks"""
-    object = Query().security()
-    object.cursor.execute("INSERT INTO players(name) VALUES(%s)", (name,))
-    object.connection.commit()
-    object.close()
+    """Add new record to players table, protecting against SQL injection attacks"""
+    Query().execute("INSERT INTO players(name) VALUES(%s)", (name,))
 
 def playerStandings():
-    """Query view to return list of players sorted by wins"""
-    object = Query().execute("SELECT * FROM standings", False)
-    results = object.cursor.fetchall()
-    object.close()
-    return results
+    """Return list of players sorted by wins from standings view"""
+    return Query().execute("SELECT * FROM standings", commit = False)
 
 def reportMatch(winner, loser):
-    """Add new record to matches, protecting against SQL injection attacks"""
-    object = Query().security()
-    object.cursor.execute("INSERT INTO matches(winner_id, loser_id) VALUES(%s, %s)", (winner, loser,))
-    object.connection.commit()
-    object.close()
- 
+    """Add new record to matches table, protecting against SQL injection attacks"""
+    Query().execute("INSERT INTO matches(winner_id, loser_id) VALUES(%s, %s)", (winner, loser,))
  
 def swissPairings():
-    """Returns a list of pairs of payers, matched according to wins"""
-    object = Query().execute("SELECT id, name FROM standings", False)
-    results = object.cursor.fetchall()
-    
-    # find out how many players there are, which gives us pairs we need to make
+    """Returns a list of pairs of payers matched according to wins"""
+    results = Query().execute("SELECT id, name FROM standings", commit = False)
     pairs = len(results)/2
+    list_pairs = []
     
-    # declare a new list to return, and tuple to hold player pairs
-    list = []
-    tuple = ()
-    
-    # iterate through the players (already sorted by wins), storing pairs in tuples and appending to the list
     for i in range(pairs):
-        tuple = results.pop() + results.pop()
-        list.append(tuple)
+        player_pair = results.pop() + results.pop()
+        list_pairs.append(player_pair)
     
-    object.close()
-    return list
+    return list_pairs
